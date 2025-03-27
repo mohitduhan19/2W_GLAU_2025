30ebecab14613ab0bbbb4af51cc6c13616d31148
Mohit Duhan <mohitkumar72956@gmail.com>
2025-03-27 10:33:31 +0530

    tree

---
+++ Lec7_Lab/Traversals.java
@@ -0,0 +1,43 @@
+package W.Lec7_Lab;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Traversals {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public void preorder(TreeNode root , List<Integer> ans){
+        if(root == null)return;
+        ans.add(root.val);
+        preorder(root.left , ans);
+        preorder(root.right , ans);
+    }
+    public void postorder(TreeNode root , List<Integer> ans){
+        if(root == null)return;
+        postorder(root.left , ans);
+        postorder(root.right , ans);
+        ans.add(root.val);
+    }
+    public void inorder(TreeNode root , List<Integer> ans){
+        if(root == null)return;
+        inorder(root.left , ans);
+        ans.add(root.val);
+        inorder(root.right , ans);
+    }
+    public List<Integer> postorder(TreeNode root) {
+        if(root == null)return new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        postorder(root , ans);
+        return ans;
+    }
+}
